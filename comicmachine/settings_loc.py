abd39181117a4c1a84aa573bc2caeac4b5e6bb35
athulkrishnan <mail@athulkrishnan.com>
2016-08-22 19:07:11 +0530

    add static

--- comicmachine/settings_loc.py
+++ comicmachine/settings_loc.py
@@ -3,8 +3,11 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(kn)n9#wace@5*k-dcm@hjf(ei6+b4vpe99&bpo^6u4=l2wts!'
 
-DEBUG = True
+DEBUG = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG404 = True 
+ALLOWED_HOSTS = ['*'] 
 
 ALLOWED_HOSTS = []
 
