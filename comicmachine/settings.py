028e4e6773d11c25aad5e5999d8f1a2a5e883df1
athulkrishnan <mail@athulkrishnan.com>
2016-08-23 13:57:25 +0530

    add whitenoise

--- comicmachine/settings.py
+++ comicmachine/settings.py
@@ -83,7 +83,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 COMPRESS_ENABLED = True
--- comicmachine/settings_loc.py
+++ comicmachine/settings_loc.py
@@ -26,3 +26,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 ADMIN_URL = 'admin/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
